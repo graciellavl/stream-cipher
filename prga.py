7aaf1bf763ed0bfd4dfdce27a06eb49bfdc789af
chintyaw <cwchintya@gmail.com>
2022-02-17 15:37:09 +0700

    add xor

--- prga.py
+++ prga.py
@@ -1,21 +1,22 @@
-def prga(S, n):
+def prga(S, text):
     # S array dari KSA
     # n panjang plaintext
 
     i = 0
     j = 0
-    key = []
-    while (n>0):
-        n -= 1
+    C = ''
+    for k in range(len(text)):
         i = (i + 1) % 256
         j = (j + S[i]) % 256
 
         #Swap
         S[i], S[j] = S[j], S[i]
 
-        K = S[(S[i] + S[j]) % 256]
-        key.append(K)
+        keystream = S[(S[i] + S[j]) % 256]
+        C += chr(ord(keystream) ^ ord(text[k]))
 
-    return key
+    return C
+
+    
 
         
