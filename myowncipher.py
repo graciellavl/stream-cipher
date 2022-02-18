fa35d5ed4069138f278cf59a90dfb11b03699cd2
graciellavl <graciellavl@users.noreply.github.com>
2022-02-18 09:12:25 +0700

    fix stream cipher

--- myowncipher.py
+++ myowncipher.py
@@ -6,7 +6,6 @@ def ksa(key):
     # membuat array "S"
     S = list(range(256))  
 
-    # Shuffle array S
     random.seed(key_length % 256)
     random.shuffle(S)
 
@@ -19,6 +18,11 @@ def prga(S, text: bytes):
     i = 0
     j = 0
     C = []
+
+    arr = []
+    for byte in text:
+        arr.append(byte)
+    
     for k in range(len(text)):
         i = (i + 1) % 256
         j = (j + S[i]) % 256
@@ -28,23 +32,23 @@ def prga(S, text: bytes):
 
         keystream = S[(S[i] + S[j]) % 256]
 
-        C.append(chr(keystream ^ text[k]))
+        # C += str(keystream.to_bytes(1, 'big') ^ ord(text[k]).to_bytes(1, 'big'))
+        C.append(keystream ^ arr[k])
 
-    return ''.join(C)
+
+    return bytearray(C)
 
 def myowncipher(key, base):
     # print(base)
     # print(type(base))
-
     # key = [ord(c) for c in key]
     # base = [ord(c) for c in base]
-
     S = ksa(key)
     hasil = prga(S, base)
     
     return hasil
 
-tes = myowncipher("abcd", "lololol")
+# tes = myowncipher("abcd", "lololol")
 
-print(myowncipher("abcd", "lololol"))
-print(myowncipher("abcd", tes))
+# print(myowncipher("abcd", "lololol"))
+# print(myowncipher("abcd", tes))
